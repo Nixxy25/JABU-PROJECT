d880e8c8619975693b4e63b98c1c343a6939187a
Tella Oyinkansola <113982462+Nixxy25@users.noreply.github.com>
2024-05-06 23:08:53 +0100

    Update Sponsorspage.jsx
    Change

--- src/components/SponsorsPage/Sponsorspage.jsx
+++ src/components/SponsorsPage/Sponsorspage.jsx
@@ -33,11 +33,11 @@ const Sponsorspage = () => {
     <div className=" px-24 py-20 max-sm:px-4 max-lg:px-10">
         <div className="text-center text-xl font-bold pb-10">340+ Leading Universities And Companies</div>
 
-        <div className="border-2 logos border-gray-300 ">
+        
       
           <Carousel responsive={responsive}  
             removeArrowOnDeviceType={["tablet", "mobile","desktop"]} 
-            className="logos-slide flex items-center justify-around"
+            className="logos-slide flex items-center p-6 border-2 logos border-gray-300 "
             autoPlay={true}
             infinite={true}
             autoPlaySpeed={1500}
@@ -53,7 +53,7 @@ const Sponsorspage = () => {
             <img className="w-32"  src={dropbox}  alt=""></img>
           </Carousel>
 
-        </div>
+        
     </div>
   )
 }
