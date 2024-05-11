159c13d98754ffde32c98989d875bfb79df300f5
Oyinkansola <tellaoyinkansola25@gmail.com>
2024-05-11 14:11:28 +0200

    changes

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
+import poolot from "../assets/poolot-logo.png";
+import Pricepage from "./pricepage";
 
 const Popup = ({onClose}) => {
 
   const [form, setForm] = useState({
     name: '',
     phoneNumber: '',
-    price: ''
+    email:"",
   });
+
   const [step, setStep] = useState(1);
+ 
 
   const handleForm = (e) => {
     const { name, value } = e.target;
@@ -18,34 +22,32 @@ const Popup = ({onClose}) => {
     setStep(step + 1);
   };
     
-  const handleFinish = () => {
-    // Handle form submission (e.g., send data to server)
-    // Show Thank You page
-    setStep(3);
-  };
+ 
+  
   return (
-    <div  className="bg-transparent h-screen w-full flex justify-center  items-center  fixed top-0 left-0 right-0 z-10">
-        <div className="bg-red-100 w-1/3 h-3/4  p-8 max-sm:w-3/4">
-          <div className="flex justify-between pb-8">
-            <h1>Popup</h1>
+    <div  className="popup-bg h-screen w-full flex justify-center  items-center inset-0 overflow-hidden  fixed top-0 left-0 right-0 z-20">
+        <div className="bg-[#fafaf9] w-1/2 h-3/4  p-8 max-sm:w-3/4">
+          <div className="flex justify-between items-center pb-8">
+            <img className="w-32" src={poolot}></img>
             <div className="flex " onClick={onClose}><button type="submit" className="bg-blue-600 rounded-md px-3 py-1 text-white">close</button></div>
           </div>
 
 
           {step === 1 && (
             <form onSubmit={handleNextStep} className="flex flex-col gap-4">
-              <input required className="border-2 border-blue-600 rounded-md outline-none p-2 bg-transparent" type="text" name="name" value={form.name} onChange={handleForm} placeholder="Name" />
-              <input required  className="border-2 border-blue-600 rounded-md outline-none p-2 bg-transparent" type="text" name="phoneNumber" value={form.phoneNumber} onChange={handleForm} placeholder="Phone Number" />
+              <input required className="border border-black rounded-md outline-none p-2 bg-transparent" type="text" name="name" value={form.name} onChange={handleForm} placeholder="Name" />
+              <input required  className="border border-black rounded-md outline-none p-2 bg-transparent" type="text" name="phoneNumber" value={form.phoneNumber} onChange={handleForm} placeholder="Phone Number" />
+              <input required  className="border border-black rounded-md outline-none p-2 bg-transparent" type="email" name="email" value={form.email} onChange={handleForm} placeholder="Email" />
               <div className="flex "><button type="submit" className="bg-blue-600 rounded-md px-3 py-1 text-white">Next</button></div>
             </form>
           )}
 
           {step === 2 && (
-            <form>
-              <input type="text" name="name" value={form.name} onChange={handleForm} placeholder="Name" />
-              <button type="submit">Finish</button>
-            </form>
+            <Pricepage handlePrice={onClose} />
           )}
+
+        
+           
         </div>
        
     </div>
---
+++ src/components/pricepage.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+
+const Pricepage = ({handlePrice}) => {
+    const [price, setPrice] = useState("");
+    const [priceError, setPriceError] = useState("");
+
+    
+
+    const handleOptionChange = (e) => {
+        setPrice(e.target.value);
+        setPriceError('')
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (price === ""){
+            setPriceError("priceError");
+        } else{
+            setPriceError("")
+            handlePrice();
+        }
+       
+        
+        
+    }
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-col justify-content items-center gap-4">
+        <label htmlFor="price" className="font-bold">Select a Price: </label>
+        <select  value={price} onChange={handleOptionChange} className="border border-black rounded-md outline-none px-4 py-1" >
+            <option value="" className="max-sm:text-sm">Enter the value</option>
+            <option value="price1">$10</option>
+            <option value="price2">$20</option>
+            <option value="price3">$30</option>
+            <option value="price4">$40</option>
+            <option value="price5">$50</option>
+        </select>
+        <div><button type="submit" className="bg-blue-600 rounded-md px-3 py-1 text-white">Finish</button></div>
+        {priceError && (<p className="text-red-500 font-bold">Select a price</p>)}   
+       
+    </form>
+    )
+    }
+
+export default Pricepage;
